798a315a5ea583841cf45e20e82ed71e61c79bdf
aryn weatherly <arynweatherly@aryns-MacBook-Pro.local>
2019-11-16 16:04:43 -0600

    fixed some stuff w/ the exercise

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace DictionaryIntro
     {
         static void Main(string[] args)
         {
-            //Declare a dictionary variable and add key value pairs to it 
-            //pass it 2 things. first is type of key, second is the type of the value 
+            //Declare a dictionary variable and add key value pairs to it
+            //pass it 2 things. first is type of key, second is the type of the value
             Dictionary<string, int> cohortCount =
                 new Dictionary<string, int>();
 
@@ -25,8 +25,10 @@ namespace DictionaryIntro
                 Console.WriteLine($"{item.Key} has {item.Value} students");
             }
 
-            //Another way to declare a dictionary 
-            Dictionary<string, double> products = new Dictionary<string, double>() { { "Deodorant", 4.5 }, { "Hair Brush", 8 }, { "Heidi Hair Brush", 5.25 }, { "Shampoo", 4.5 }, { "Tooth Brush", 40 } };
+            //Another way to declare a dictionary
+            Dictionary<string, double> products = new Dictionary<string, double>()
+            { { "Deodorant", 4.5 }, { "Hair Brush", 8 }, { "Heidi Hair Brush", 5.25 }, { "Shampoo", 4.5 }, { "Tooth Brush", 40 }
+            };
 
             //want to iterate over these and write to console something like deodorant cost 4.50
 
